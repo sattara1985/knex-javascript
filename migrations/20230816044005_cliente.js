a9ad17a6dfe388fb7ebfc76cc22490508a986cf7
steven attara <sattara1985@gmail.com>
2023-08-15 23:49:12 -0500

    migraciones creadas

--- migrations/20230816044005_cliente.js
+++ migrations/20230816044005_cliente.js
@@ -3,6 +3,17 @@
  * @returns { Promise<void> }
  */
 exports.up = function(knex) {
+    return knex.schema.hasTable('cliente').then((exists) => {
+        if (!exists) {
+            return knex.schema.createTable("cliente", function (table) {
+            table.increments("dni").primary();
+            table.string("nombre").notNullable();
+            table.string("primer_apellido").notNullable();
+            table.boolean("active").notNullable().defaultTo(true);
+            table.timestamp("created_at").defaultTo(knex.fn.now());
+            });
+        }
+    });
   
 };
 
